86b2f4df4b3664c6035fa14aa616d87a4374d378
zh-ray <wxjiooio@gmail.com>
2021-01-28 22:10:01 +0800

    some mocking code

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,12 +4,57 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+type Sleeper interface {
+	Sleep()
+}
+
+// type SpySleeper struct {
+// 	Calls int
+// }
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+// func (s *SpySleeper) Sleep() {
+// 	s.Calls++
+// }
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+const finalword = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalword)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
 
-	Countdown(buffer)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationSpy{})
 
-	got := buffer.String()
-	want := "3"
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySleeperPrinter := &CountdownOperationSpy{}
+		Countdown(spySleeperPrinter, spySleeperPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleeperPrinter.Calls) {
+			t.Errorf("want calls %v got %v", want, spySleeperPrinter.Calls)
+		}
+	})
 }
