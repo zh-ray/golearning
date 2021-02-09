a7768f5b488174d43215cb9428056540b17a8770
zh-ray <wxjiooio@gmail.com>
2021-02-09 17:25:00 +0800

    add Sync and Context code

---
+++ context/server.go
@@ -0,0 +1,17 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, store.Fetch())
+	}
+}
---
+++ sync/sync.go
@@ -0,0 +1,22 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.Lock()
+	defer c.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
